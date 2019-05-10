7200cba656554e82a6a9c7fd877bb0b09c156f9e
Reid Klein <reidklein@hotmail.com>
2019-05-10 00:20:01 -0500

    fix json truncating issue

--- FreeWheel.MovieDb.Api/Startup.cs
+++ FreeWheel.MovieDb.Api/Startup.cs
@@ -26,7 +26,8 @@ namespace FreeWheel.MovieDb.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
+                    .AddJsonOptions(options => options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore); //fix truncate issue with .net core
 
             //services
             services.AddTransient<IMoviesService, MoviesService>();
